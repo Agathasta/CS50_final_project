b242232029cede2fb04c5f3cc815f0e4b37e72b6
agathasta <kalidonia@hotmail.com>
2021-03-06 23:41:18 +0100

    Last commit before complete aproach change (before I drown)

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,4 @@
 // // *** https://www.w3schools.com/howto/howto_js_todolist.asp ***
-// // Create a "close" button and append it to each list item
-// var myNodelist = document.querySelector('.todos');
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
 
 // // Add a "checked" symbol when clicking on a list item
 // var list = document.querySelector('#day-list');
@@ -28,31 +18,3 @@
 //   }
 // }
 
-
-
-// // Create a new list item when clicking on the "Add" button
-// // function newElement() {
-// //   var li = document.createElement("li");
-// //   var inputValue = document.getElementById("myInput").value;
-// //   var t = document.createTextNode(inputValue);
-// //   li.appendChild(t);
-// //   if (inputValue === '') {
-// //     alert("You must write something!");
-// //   } else {
-// //     document.getElementById("myUL").appendChild(li);
-// //   }
-// //   document.getElementById("myInput").value = "";
-
-// //   var span = document.createElement("SPAN");
-// //   var txt = document.createTextNode("\u00D7");
-// //   span.className = "close";
-// //   span.appendChild(txt);
-// //   li.appendChild(span);
-
-// //   for (i = 0; i < close.length; i++) {
-// //     close[i].onclick = function () {
-// //       var div = this.parentElement;
-// //       div.style.display = "none";
-// //     }
-// //   }
-// //}
